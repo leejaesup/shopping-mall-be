0acffa024065a8cea1a7983c5e8ec856d930e3e5
js <leejaesup@naver.com>
2024-06-04 20:53:30 +0900

    페이지네이션 완료

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -1,5 +1,6 @@
 const Product = require("../model/Product");
 
+const PAGE_SIZE = 5;
 const productController = {};
 
 //상품 생성
@@ -20,9 +21,32 @@ productController.createProduct = async (req, res) => {
 //유저정보 가져오기
 productController.getProduct = async (req, res) => {
     try {
-        const products = await Product.find({});
+        const {page, name} = req.query;
+
+        // let productList;
+        // if (name) {
+        //     productList = await Product.find({name: {$regex:name, $option:"i"}});
+        // } else {
+        //     productList = await Product.find({});
+        // }
+        const condition = name ? {name: {$regex: name, $options: "i"}} : {};
+        let query = Product.find(condition);
+        let response = {state: "success"};
+
+        if (page) {
+            query.skip((page - 1) * PAGE_SIZE).limit(PAGE_SIZE);
+            // 전체 페이지 수
+            const totalItemNum = await Product.find(condition).count();
+            const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
+
+            response.totalPageNum = totalPageNum;
+        }
+        const productList = await query.exec();
+        response.data = productList;
+
         //200 Ok
-        res.status(200).json({status: "success", data: products});
+        // res.status(200).json({status: "success", data: productList});
+        res.status(200).json(response);
     } catch (error) {
         //400 Bad Request
         res.status(400).json({status: "fail", message: error.message});
