800a502380e93bf059545237eea8fa55a3b3d4d7
js <leejaesup@naver.com>
2024-06-07 20:48:27 +0900

    chore: OAuth2 추가(Google)

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -3,8 +3,11 @@ const jwt = require('jsonwebtoken');
 const User = require("../model/User");
 const bcrypt = require("bcryptjs");
 const userController = require("./user.controller");
+const {OAuth2Client} = require("google-auth-library");
+const e = require("express");
 require('dotenv').config();
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 
 
 //로그인(POST)
@@ -25,6 +28,46 @@ authController.loginWithEmail = async (req, res) => {
         }
 
         throw new Error('email 또는 비밀번호가 일치하지 않습니다.');
+    } catch (error) {
+        //400 Bad Request
+        res.status(400).json({status: "fail", message: error.message});
+    }
+}
+
+authController.loginWithGoogle = async (req, res) => {
+    try {
+        const {token} = req.body;
+        const googleClient = new OAuth2Client(GOOGLE_CLIENT_ID);
+
+        const ticket = await googleClient.verifyIdToken({
+            idToken: token,
+            audience: GOOGLE_CLIENT_ID
+        });
+
+        const {email, name} = ticket.getPayload();
+
+        // 존재하는 계정인지 확인
+        let user = await User.findOne({email});
+        console.log("user = ", user);
+
+        if (!user) {
+            // 유저를 새로 생성
+            const randomPassword = "" + Math.floor(Math.random() * 1000000);
+            const salt = await bcrypt.genSalt(10);
+            const newPassword = await bcrypt.hash(randomPassword, salt);
+
+            user = new User({
+                name,
+                email,
+                password: newPassword
+            });
+            await user.save();
+        }
+
+        //토큰 발행
+        const sessionToken = await user.generateToken();
+        //200 Ok
+        return res.status(200).json({status: "success", user, token: sessionToken});
     } catch (error) {
         //400 Bad Request
         res.status(400).json({status: "fail", message: error.message});
