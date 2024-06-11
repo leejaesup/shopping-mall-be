82005b3e1539f0e4b621e09c9ac500adb16a315f
Jaeseop Lee <leejaesup@naver.com>
2024-06-12 00:28:30 +0900

    fix: 재고 수정

--- controller/cart.controller.js
+++ controller/cart.controller.js
@@ -55,37 +55,6 @@ cartController.getCart = async (req, res) => {
     }
 };
 
-// cartController.editCartItem = async (req, res) => {
-//     try {
-//         const {userId } = req;
-//         const productId = req.params.id;
-//         const {size, qty } = req.body;
-//
-//         const cart = await Cart.findOne({userId});
-//         if (!cart) {
-//             throw new Error("카트가 비어있습니다.");
-//         }
-//
-//         const index = cart.items.findIndex((item) =>
-//             item.productId.equals(productId)
-//         );
-//
-//         if (index === -1) {
-//             throw new Error("존재하지 않는 아이템입니다.");
-//         }
-//
-//         cart.items[index].size = size;
-//         cart.items[index].qty = qty;
-//
-//         await cart.save();
-//
-//         //200 Ok
-//         return res.status(200).json({status: "ok", data: cart.items});
-//     } catch (error) {
-//         //400 Bad Request
-//         res.status(400).json({status: "fail", message: error.message});
-//     }
-// };
 cartController.editCartItem = async (req, res) => {
     try {
         const { userId } = req;
@@ -99,9 +68,20 @@ cartController.editCartItem = async (req, res) => {
                 model: "Product",
             },
         });
-        if (!cart) throw new Error("There is no cart for this user");
+
+        if (!cart) {
+            throw new Error("카트가 비었습니다.");
+        }
+
         const index = cart.items.findIndex((item) => item._id.equals(id));
-        if (index === -1) throw new Error("Can not find item");
+        if (index === -1) {
+            throw new Error("제품을 찾을 수 없습니다.");
+        }
+
+        if (cart.items[index].qty <= qty) {
+            throw new Error("재고 수량보다 많을 수 없습니다.");
+        }
+
         cart.items[index].qty = qty;
         await cart.save();
         res.status(200).json({ status: 200, data: cart.items });
@@ -110,37 +90,11 @@ cartController.editCartItem = async (req, res) => {
     }
 };
 
-// cartController.deleteCartItem = async (req, res) => {
-//     try {
-//         const {userId } = req;
-//         const productId = req.params.id;
-//
-//         const cart = await Cart.findOne({userId});
-//         if (!cart) {
-//             throw new Error("카트가 비어있습니다.");
-//         }
-//
-//         const index = cart.items.findIndex((item) =>
-//             item.productId.equals(productId)
-//         );
-//         if (index === -1) {
-//             throw new Error("존재하지 않는 아이템입니다.");
-//         }
-//         cart.items.splice(index, 1);
-//         await cart.save();
-//
-//         //200 Ok
-//         return res.status(200).json({status: "ok", data: cart.items, cartItemQty: cart.items.length});
-//     } catch (error) {
-//         //400 Bad Request
-//         res.status(400).json({status: "fail", message: error.message});
-//     }
-// };
 cartController.deleteCartItem = async (req, res) => {
     try {
-        const { id } = req.params;
-        const { userId } = req;
-        const cart = await Cart.findOne({ userId });
+        const {id} = req.params;
+        const {userId} = req;
+        const cart = await Cart.findOne({userId});
         cart.items = cart.items.filter((item) => !item._id.equals(id));
 
         await cart.save();
